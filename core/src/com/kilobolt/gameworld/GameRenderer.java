2a9c7b13aa4ccf984d6493e6ee7f83d99bf344d3
John Curton <ivan-horak@hotmail.com>
2015-11-16 21:45:37 +0100

    Initial Commit

--- core/src/com/kilobolt/gameworld/GameRenderer.java
+++ core/src/com/kilobolt/gameworld/GameRenderer.java
@@ -1,8 +1,22 @@
 package com.kilobolt.gameworld;
 
+import com.badlogic.gdx.Gdx;
+
 /**
  * Created by ivan- on 16.11.2015..
  */
 public class GameRenderer {
-    //comment
+
+    private GameWorld myWorld;
+
+    public GameRenderer(GameWorld world) {
+        myWorld=world;
+
+    }
+
+    public void render() {
+
+        Gdx.app.log("GameRenderer","render");
+    }
+
 }
--- core/src/com/kilobolt/gameworld/GameWorld.java
+++ core/src/com/kilobolt/gameworld/GameWorld.java
@@ -1,7 +1,13 @@
 package com.kilobolt.gameworld;
 
+import com.badlogic.gdx.Gdx;
+
 /**
  * Created by ivan- on 16.11.2015..
  */
 public class GameWorld {
+    public void update(float delta) {
+
+        Gdx.app.log("GameWorld","update");
+    }
 }
