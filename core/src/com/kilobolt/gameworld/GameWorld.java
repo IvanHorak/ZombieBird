e3500aa6f8eede1352b670c1758af7c72c35b414
John Curton <ivan-horak@hotmail.com>
2015-11-16 23:00:27 +0100

    Making a rectangle drive across the screen

--- core/src/com/kilobolt/gameworld/GameWorld.java
+++ core/src/com/kilobolt/gameworld/GameWorld.java
@@ -1,13 +1,22 @@
 package com.kilobolt.gameworld;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.math.Rectangle;
 
-/**
- * Created by ivan- on 16.11.2015..
- */
+
 public class GameWorld {
+
+    private Rectangle rect = new Rectangle(0,0,20,20);
+
     public void update(float delta) {
 
+        rect.x++;
+        if (rect.x>137)
+            rect.x=0;
         Gdx.app.log("GameWorld","update");
+    }
+
+    public Rectangle getRect() {
+        return rect;
     }
 }
