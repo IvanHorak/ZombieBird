27a75e238ea4c89e37e89acd75f7e8b56acb355a
John Curton <ivan-horak@hotmail.com>
2015-11-17 03:41:22 +0100

    Implemented scrolling and displaying of towers and grass

--- core/src/com/kilobolt/gameworld/GameRenderer.java
+++ core/src/com/kilobolt/gameworld/GameRenderer.java
@@ -8,6 +8,9 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.kilobolt.gameobjects.Bird;
+import com.kilobolt.gameobjects.Grass;
+import com.kilobolt.gameobjects.Pipe;
+import com.kilobolt.gameobjects.ScrollHandler;
 import com.kilobolt.zbHelpers.AssetLoader;
 
 public class GameRenderer {
@@ -22,6 +25,9 @@ public class GameRenderer {
     private int gameHeight;
 
     private Bird bird;
+    private ScrollHandler scroller;
+    private Grass frontGrass, backGrass;
+    private Pipe pipe1, pipe2, pipe3;
 
     private TextureRegion bg, grass;
     private Animation birdAnimation;
@@ -42,6 +48,12 @@ public class GameRenderer {
 
     private void initGameObjects() {
         bird = myWorld.getBird();
+        scroller = myWorld.getScroller();
+        frontGrass = scroller.getFrontGrass();
+        backGrass = scroller.getBackGrass();
+        pipe1 = scroller.getPipe1();
+        pipe2 = scroller.getPipe2();
+        pipe3 = scroller.getPipe3();
     }
 
     public GameRenderer(GameWorld world, int gameHeight, int midPointY) {
@@ -64,6 +76,54 @@ public class GameRenderer {
         initAssets();
 
     }
+
+    private void drawGrass() {
+        // Draw the grass
+        batcher.draw(grass, frontGrass.getX(), frontGrass.getY(),
+                frontGrass.getWidth(), frontGrass.getHeight());
+        batcher.draw(grass, backGrass.getX(), backGrass.getY(),
+                backGrass.getWidth(), backGrass.getHeight());
+    }
+
+    private void drawSkulls() {
+        // Temporary code! Sorry about the mess :)
+        // We will fix this when we finish the Pipe class.
+
+        batcher.draw(skullUp, pipe1.getX() - 1,
+                pipe1.getY() + pipe1.getHeight() - 14, 24, 14);
+        batcher.draw(skullDown, pipe1.getX() - 1,
+                pipe1.getY() + pipe1.getHeight() + 45, 24, 14);
+
+        batcher.draw(skullUp, pipe2.getX() - 1,
+                pipe2.getY() + pipe2.getHeight() - 14, 24, 14);
+        batcher.draw(skullDown, pipe2.getX() - 1,
+                pipe2.getY() + pipe2.getHeight() + 45, 24, 14);
+
+        batcher.draw(skullUp, pipe3.getX() - 1,
+                pipe3.getY() + pipe3.getHeight() - 14, 24, 14);
+        batcher.draw(skullDown, pipe3.getX() - 1,
+                pipe3.getY() + pipe3.getHeight() + 45, 24, 14);
+    }
+
+    private void drawPipes() {
+        // Temporary code! Sorry about the mess :)
+        // We will fix this when we finish the Pipe class.
+        batcher.draw(bar, pipe1.getX(), pipe1.getY(), pipe1.getWidth(),
+                pipe1.getHeight());
+        batcher.draw(bar, pipe1.getX(), pipe1.getY() + pipe1.getHeight() + 45,
+                pipe1.getWidth(), midPointY + 66 - (pipe1.getHeight() + 45));
+
+        batcher.draw(bar, pipe2.getX(), pipe2.getY(), pipe2.getWidth(),
+                pipe2.getHeight());
+        batcher.draw(bar, pipe2.getX(), pipe2.getY() + pipe2.getHeight() + 45,
+                pipe2.getWidth(), midPointY + 66 - (pipe2.getHeight() + 45));
+
+        batcher.draw(bar, pipe3.getX(), pipe3.getY(), pipe3.getWidth(),
+                pipe3.getHeight());
+        batcher.draw(bar, pipe3.getX(), pipe3.getY() + pipe3.getHeight() + 45,
+                pipe3.getWidth(), midPointY + 66 - (pipe3.getHeight() + 45));
+    }
+
 
     public void render(float runTime) {
 
@@ -100,9 +160,18 @@ public class GameRenderer {
         // The bird needs transparency, so we enable that again.
         batcher.enableBlending();
 
-        // Draw bird at its coordinates. Retrieve the Animation object from
-        // AssetLoader
-        // Pass in the runTime variable to get the current frame.
+
+        // 1. Draw Grass
+        drawGrass();
+
+        // 2. Draw Pipes
+        drawPipes();
+        batcher.enableBlending();
+
+        // 3. Draw Skulls (requires transparency)
+        drawSkulls();
+
+
         if (bird.shouldntFlap()) {
             batcher.draw(birdMid, bird.getX(), bird.getY(),
                     bird.getWidth() / 2.0f, bird.getHeight() / 2.0f,
